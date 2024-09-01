e3c19e3a839de0d640b888d6d153be2035f3ec37
AbuRahat889 <aburahatshaum889@gmail.com>
2024-09-01 23:10:53 +0600

    add middleware

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const AboutPage = () => {
-    // console.log(name);
-    return (
-        <div>
-            <h1>this is about page </h1>
-            <p>contact me </p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>this is about page </h1>
+      <p>contact me </p>
+    </div>
+  );
 };
 
 export default AboutPage;
